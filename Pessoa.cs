e59d90ae61ba4135277181771216d8522db5c4b4
maicon-oliveira <maicon.oliveira0702@gmail.com>
2021-10-07 20:55:50 -0300

    commit inicial

---
+++ Pessoa.cs
@@ -0,0 +1,16 @@
+namespace CadastroPessoa
+{
+    public abstract class Pessoa 
+    {
+        private string nome { get; set; }
+
+        private string endereco { get; set; }
+
+        private bool endResidencia { get; set; }
+
+        public void pagarImposto(){
+
+        }
+ 
+    }
+}
---
+++ PessoaJuridica.cs
@@ -0,0 +1,12 @@
+namespace CadastroPessoa
+{
+    public class PessoaJuridica : Pessoa
+    {
+        private string cnpj { get; set; }
+
+        private string razaoSocial { get; set; }
+        
+                
+        
+    }
+}
